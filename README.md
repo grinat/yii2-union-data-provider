8428d71922a57123f99b9b25e16f3b75df1ef459
grinat <paladin2012gnu@gmail.com>
2018-04-25 21:50:57 +0300

    package init

---
+++ README.md
@@ -0,0 +1,112 @@
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist grinat/yii2-union-data-provider
+```
+
+or add
+
+```
+"grinat/yii2-union-data-provider": "^0.1.0"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+------------
+```
+use grinat\uniondataprovider\UnionDataProvider;
+use yii\base\Model;
+
+class ListOfActivityUnionModel extends Model{
+
+    public function search()
+    {
+        $dataProvider = new UnionDataProvider([
+            // model which attributes use for sort data
+            'searchModel' => $this,
+            'options' => [
+                // needed fields
+                'select' => 'id,comment,created_at,user_id',
+                'union' => [
+                    [
+                         // set in class path to model
+                        'class' => 'app\modules\recipe\models\RecipeComment',
+                        // aliases for fields which have diiferent name
+                        // was return in that example as relationColumn
+                        // if the names are the same, specify them in select
+                        'selectAs' => ['recipe_id' => 'relationColumn'],
+                        // set relation of you need corresponds to $query->width('createdUser')
+                        // if in relation used column with alias, set it as [alias => relationName]
+                        'withRel' => ['createdUser', 'relationColumn' => 'recipe']
+                    ],
+                    [
+                        'class' => 'app\modules\coach\models\CoachDietReview',
+                        'selectAs' => ['diet_id' => 'relationColumn'],
+                        'withRel' => ['createdUser', 'relationColumn' => 'diet']
+                    ],
+                    [
+                        'class' => 'app\modules\coach\models\CoachReview',
+                        'selectAs' => ['coach_id' => 'relationColumn'],
+                        'withRel' => ['createdUser', 'relationColumn' => 'coach.createdUser']
+                    ]
+                ]
+            ],
+            'sort' => [
+                'defaultOrder' => ['created_at' => SORT_DESC],
+            ],
+            // string Set null if need primaryKey for relWith from data scheme
+            // 'unionRelsPk' => 'id', 
+            // Return data from provider As array or not 
+            // 'asArray' => false,
+            // Set false if dont need duplicates
+            // 'unionAll' => false, 
+            // if needed correct data-key in GrigView and etc
+            // 'key' => 'id' 
+        ]);
+
+        return $dataProvider;
+    }
+}
+```
+The execute query is:
+```
+SELECT * 
+FROM   ((SELECT "id", 
+                "comment", 
+                "created_at", 
+                "user_id", 
+                Concat('0') AS union_index, 
+                "recipe_id" AS "relationColumn" 
+         FROM   "recipe_comment") 
+        UNION ALL 
+        (SELECT "id", 
+                "comment", 
+                "created_at", 
+                "user_id", 
+                Concat('1') AS union_index, 
+                "diet_id"   AS "relationColumn" 
+         FROM   "coach_diet_review") 
+        UNION ALL 
+        (SELECT "id", 
+                "comment", 
+                "created_at", 
+                "user_id", 
+                Concat('2') AS union_index, 
+                "coach_id"  AS "relationColumn" 
+         FROM   "coach_review")) "dummy_table" 
+ORDER  BY "created_at" DESC 
+LIMIT  10 
+```
+With queries
+```sql
+SELECT * FROM "user" WHERE "id" IN (5, 13, 3)
+SELECT * FROM "recipe_recipe" WHERE "id" IN (1, 2, 18, 9)
+...etc
+```
---
+++ src/UnionDataProvider.php
@@ -0,0 +1,326 @@
+<?php
+/**
+ * @email paladin2012gnu@gmail.com
+ * @author Gabdrashitov Rinat
+ */
+namespace grinat\uniondataprovider;
+
+use yii\data\ActiveDataProvider;
+use yii\data\BaseDataProvider;
+use yii\db\Query;
+
+/**
+ * Use example
+ * 
+ * class ListOfActivityUnionModel extends Model{
+
+ * public $id;
+ * public $created_at;
+ *
+ *  public function search(){
+ * 
+ *       $dataProvider = new UnionDataProvider([
+ *            'searchModel' => $this,
+ *            'options' => $this->options,
+ *            'sort' => [
+ *               'defaultOrder' => ['created_at' => SORT_DESC],
+ *            ],
+ *            // if needed correct data-key in GrigView and etc
+ *            // 'key' => 'id' 
+ *       ]);
+ *       
+ *       return $dataProvider;
+ *   }
+ * 
+ * }
+ */
+
+class UnionDataProvider extends ActiveDataProvider { /*extends BaseDataProvider implements DataProviderInterface{*/
+
+    /**
+     * @var array Example of config options
+     *    [
+     *        'select' => 'id,comment,created_at,user_id',
+     *        'union' => [
+     *            [
+     *               'class' => 'app\modules\coach\models\CoachDietComment',
+     *               'selectAs' => ['diet_id' => 'relationColumn'],
+     *               'withRel' => ['createdUser', 'relationColumn' => 'diet']
+     *            ],
+     *            [
+     *                'class' => 'app\modules\coach\models\CoachDietReview',
+     *                'selectAs' => ['diet_id' => 'relationColumn'],
+     *                'withRel' => ['createdUser', 'relationColumn' => 'diet']
+     *            ],
+     *            [
+     *                'class' => 'app\modules\coach\models\CoachReview',
+     *                'selectAs' => ['coach_id' => 'relationColumn'],
+     *                'withRel' => ['createdUser', 'relationColumn' => 'coach.createdUser']
+     *            ]
+     *        ]
+     *    ]
+     */
+    public $options;
+    
+    /**
+     *
+     * @var boolen Return data from provider As array or not 
+     */
+    public $asArray = false;
+    
+    /**
+     *
+     * @var boolen Set false if dont need duplicates
+     */
+    public $unionAll = true;
+
+    /**
+     * @var \yii\base\Model virtual model
+     * which attributes use for sort data
+     */
+    public $searchModel;
+    
+    /**
+     * @var string Set null if need primaryKey for relWith from data scheme
+     */
+    public $unionRelsPk = 'id';
+
+    public function init()
+    {
+        parent::init();
+        
+        if(!isset($this->options['select'])){
+            $this->options['select'] = '';
+        }
+        
+        $mainQ = null;
+        foreach($this->options['union'] as $index => $option){
+            $columns = [];
+            if(is_array($this->options['select'])){
+                $columns = $this->options['select'];
+            } else {
+                if($this->options['select']){
+                    $columns = explode(',', $this->options['select']);
+                }
+            }
+            // little hack, else yii2 quted index
+            $columns[] = 'concat(\''.$index.'\') AS union_index';
+            if(isset($option['selectAs'])){
+                foreach ($option['selectAs'] as $col => $colAs) {
+                    $columns[] = $col . ' AS ' . $colAs;
+                }
+            }
+            /* @var $model \yii\db\ActiveRecord */
+            $model = new $option['class'];
+            $q = $model::find()->select($columns);
+            if($mainQ === null){
+                $mainQ = $q;
+            } else {
+                $mainQ->union($q, $this->unionAll);
+            }
+        }
+        /* @var query \yii\db\Query */
+        $this->query = (new Query())->from(['dummy_table' => $mainQ]);
+        
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    protected function prepareModels()
+    {
+        $dataArr = parent::prepareModels();
+
+        // get relations info
+        $relationsParamsByRelation = [];
+        $relationsDataKeys = [];
+        $relationsOfWithRel = [];
+        $relationsPk = [];
+        foreach($this->options['union'] as $option){
+            if (isset($option['withRel'])) {
+                $modelClass = $option['class'];
+                /* @var $model \yii\db\ActiveRecord */
+                $model = new $modelClass;
+                foreach ($option['withRel'] as $relKey => $relName) {
+                    if(strpos($relName, '.') !== false){
+                        $childs = explode('.', $relName);
+                        $relName = $childs[0];
+                        unset($childs[0]);
+                        $relationsOfWithRel[$relName] = implode('.', $childs);
+                    }
+                    $relationsParamsByRelation[$relName] = $model->getRelation($relName);
+                    if($this->unionRelsPk === null){
+                        $relationsPk[$relName] = $model::primaryKey()[0];
+                    }else{
+                        $relationsPk[$relName] = $this->unionRelsPk;
+                    }
+                    if (is_numeric($relKey)) {
+                        $relationsDataKeys[$relName] = $relationsParamsByRelation[$relName]->link[$relationsPk[$relName]];
+                    } else {
+                        $relationsDataKeys[$relName] = $relKey;
+                    }
+                }
+            }
+        }
+        
+        // get id from relations
+        $relationsIds = [];
+        foreach($dataArr as $data){
+            foreach($relationsParamsByRelation as $relName => $param){
+                $dataKey = $relationsDataKeys[$relName];
+                if(isset($data[$dataKey])){
+                    $relationsIds[$relName][] = $data[$dataKey]; 
+                } 
+            }
+        }
+        
+        // get relations
+        $relations = [];
+        $relationsModelKeys = [];
+        foreach($relationsParamsByRelation as $relName => $param){
+            if(!isset($relationsIds[$relName])){
+                continue;
+            }
+            
+            $where = [];
+            foreach($param->link as $fieldId => $fieldName){
+                if($fieldId == $relationsPk[$relName]){
+                    $relationsModelKeys[$relName] = $fieldName;
+                    $where[$fieldId] = array_unique($relationsIds[$relName]);
+                    $idField = $fieldId;
+                }
+            }
+            if(is_array($param->where)){
+                $where = array_merge($where, $param->where);
+            }
+            /* @var $class \yii\db\ActiveRecord */
+            $class = $param->modelClass;
+            $q = $class::find()
+                    ->where($where)
+                    ->indexBy($idField)
+                    ->asArray($this->asArray);
+            
+            if(isset($relationsOfWithRel[$relName])){
+                $q->with($relationsOfWithRel[$relName]);
+            }
+            
+            $relations[$relName] = $q->all();
+        }
+
+        //print_r($relations);die;
+        // prepare models
+        $models = [];
+        foreach($dataArr as $data){
+            $option = $this->options['union'][$data['union_index']];
+            
+            if (isset($option['selectAs'])) {
+                foreach ($option['selectAs'] as $col => $colAs) {
+                    $data[$col] = $data[$colAs];
+                }
+            }
+
+            if($this->asArray === false){
+                /* @var $model \yii\db\ActiveRecord */
+                $model = new $option['class'];
+                $model->setAttributes($data, false);
+            } else {
+                $model = $data;
+            }
+            
+            if (!isset($option['withRel'])) {
+                $models[] = $model;
+                continue;
+            }
+            
+            // set relations
+            foreach($option['withRel'] as $relKey => $relName){
+                // echo "$relKey => $relName  ".$relationsModelKeys[$relName]."\n";
+                if(strpos($relName, '.') !== false){
+                    $childs = explode('.', $relName);
+                    $relName = $childs[0];
+                }
+                
+                if(!isset($relationsModelKeys[$relName])){
+                    continue;
+                }
+                $modelRelId = $relationsModelKeys[$relName];
+                if($this->asArray === false){
+                    if (isset($model->$modelRelId)) {
+                        if(isset($relations[$relName][$model->$modelRelId])){
+                            $model->populateRelation($relName, $relations[$relName][$model->$modelRelId]);
+                        }else{
+                            $model->populateRelation($relName, null);
+                        }
+                    }
+                } else {
+                    if (isset($model[$modelRelId])) {
+                        if(isset($relations[$relName][$model[$modelRelId]])){
+                            $model[$relName] = $relations[$relName][$model[$modelRelId]];
+                        } else {
+                            $model[$relName] = null;
+                        }
+                    }
+                }
+            }
+
+            $models[] = $model;
+        }
+        
+        // print_r($models);die;
+        return $models;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function prepareTotalCount()
+    {
+        return parent::prepareTotalCount();
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    protected function prepareKeys($models)
+    {
+       
+        $keys = [];
+        if ($this->key !== null) {
+            foreach ($models as $model) {
+                if (is_string($this->key)) {
+                    $keys[] = $model[$this->key];
+                } else {
+                    $keys[] = call_user_func($this->key, $model);
+                }
+            }
+
+            return $keys;
+        } 
+
+        return array_keys($models);
+    }
+
+    public function setSort($value)
+    {
+        BaseDataProvider::setSort($value);
+        if (($sort = $this->getSort()) !== false) {
+            /* @var $model \yii\base\Model */
+            $model = $this->searchModel;
+            if (empty($sort->attributes)) {
+                foreach ($model->attributes() as $attribute) {
+                    $sort->attributes[$attribute] = [
+                        'asc' => [$attribute => SORT_ASC],
+                        'desc' => [$attribute => SORT_DESC],
+                        'label' => $model->getAttributeLabel($attribute),
+                    ];
+                }
+            } else {
+                foreach ($sort->attributes as $attribute => $config) {
+                    if (!isset($config['label'])) {
+                        $sort->attributes[$attribute]['label'] = $model->getAttributeLabel($attribute);
+                    }
+                }
+            }
+        }
+    }
+}
